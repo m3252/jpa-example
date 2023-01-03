e39a0f7acb58ad3de77d6581728f05d9b54c7e10
seungchan.moon <moon.unimewo@gmail.com>
2023-01-03 18:32:41 +0900

    feat: jpa dirty checking

---
+++ ex1-hello-jpa/src/main/java/jpabasic/JpaDirtyChecking.java
@@ -0,0 +1,31 @@
+package jpabasic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaDirtyChecking {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = em.find(Member.class, 150L);
+            member.setName("150번바꿈");
+
+            //em.persist(member);
+
+            System.out.println("========= after setName ========= ");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
