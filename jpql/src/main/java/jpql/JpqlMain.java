51a6732da04d5455f84b51a129d80fb2c32d868c
noah <m3252@naver.com>
2023-04-16 18:35:36 +0900

    feat: jpql 페이징, 프로젝션 개념

--- jpql/src/main/java/jpql/JpqlMain.java
+++ jpql/src/main/java/jpql/JpqlMain.java
@@ -1,9 +1,9 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jpql.domain.Member;
+
+import javax.persistence.*;
+import java.util.List;
 
 public class JpqlMain {
 
@@ -13,6 +13,26 @@ public class JpqlMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("resultList.size() = " + resultList.size());
+            resultList.forEach(m -> System.out.println("m = " + m.getAge()));
+
+
 
             tx.commit();
         } catch (Exception e) {
