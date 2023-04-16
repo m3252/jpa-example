250a115db33656c9b39ada8254dc3d05e58da360
noah <m3252@naver.com>
2023-04-16 16:11:40 +0900

    refactor: 실전예제 주소 관련 필드 valueObject 전환

--- ex1-hello-jpa/src/main/java/jpashop/domain/Member.java
+++ ex1-hello-jpa/src/main/java/jpashop/domain/Member.java
@@ -1,5 +1,7 @@
 package jpashop.domain;
 
+import graph.domain.Address;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +13,9 @@ public class Member extends BaseEntity {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
@@ -34,28 +36,12 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public List<Order> getOrders() {
