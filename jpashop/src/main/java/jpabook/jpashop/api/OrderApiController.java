c93dc6af3cabd4e3e2f1829d074cff9ce1efe4bd
noah <m3252@naver.com>
2023-05-06 00:24:03 +0900

    feat: 컬렉션 주문 조회 V3.1: 페이징과 한계 돌파

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -45,6 +46,19 @@ public class OrderApiController {
     @GetMapping("api/v3/orders")
     public List<OrderResponse> orderV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderResponse> collect = orders.stream()
+                .map(o -> new OrderResponse(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    @GetMapping("api/v3.1/orders")
+    public List<OrderResponse> orderV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberAndDelivery(offset, limit);
         List<OrderResponse> collect = orders.stream()
                 .map(o -> new OrderResponse(o))
                 .collect(Collectors.toList());
