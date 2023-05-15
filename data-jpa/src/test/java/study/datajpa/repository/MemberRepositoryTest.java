713c5fd8e3c3e2de7deff22272eae96d7f1b40e5
noah <m3252@naver.com>
2023-05-15 18:59:57 +0900

    feat: 메소드명 조합으로 쿼리 생성

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -60,5 +60,26 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void findHellBy() {
+        List<Member> helloBy = memberRepository.findTop3HelloBy();
+
+    }
+
 
 }
