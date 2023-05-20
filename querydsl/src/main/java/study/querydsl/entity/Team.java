6d40b9d210df3b595ce1b061120658cc93a4faab
noah <m3252@naver.com>
2023-05-20 16:11:31 +0900

    feat/test: domain Member, Team

---
+++ querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,31 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    protected Team() {}
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
