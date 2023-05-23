4fa577002a262e2cc38d66d590ceafc9f89c92e1
noah <m3252@naver.com>
2023-05-23 19:09:56 +0900

    refactor/test: spring data jpa 연결
    - add custom repository
    - pageable
    - add paging api

---
+++ querydsl/src/main/java/study/querydsl/repository/MemberQueryRepository.java
@@ -0,0 +1,63 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQueryFactory;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+import java.util.List;
+
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+/**
+ * 많이 복잡한 쿼리면 적당한 분리도 필요
+ */
+
+@Repository
+public class MemberQueryRepository {
+
+    private final JPQLQueryFactory queryFactory;
+
+    public MemberQueryRepository(JPQLQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .join(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return teamName != null ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return username != null ? member.username.eq(username) : null;
+    }
+}
