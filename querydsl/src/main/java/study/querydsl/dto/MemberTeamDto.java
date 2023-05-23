2aaf512439665213e316b1ab708e645e97b62847
noah <m3252@naver.com>
2023-05-23 18:04:09 +0900

    feat/chore: 회원과 팀 조회 API 추가, 환경 별 프로퍼티 분리
    - MemberJpaRepository (jpa, querydsl)
    - 동적 쿼리 메소드 추가
    - add PostConstruct

---
+++ querydsl/src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,22 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    @QueryProjection // Querydsl에서 DTO로 바로 조회할 때 사용
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
